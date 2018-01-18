f3dde43ed2dfe0455c32b19f6f8968c96ec5fa38
Colin <colinm1215@gmail.com>
2018-01-18 12:12:26 -0500

    Proton
    Add Skeletons of Proton, upQuark, and downQuark

---
+++ src/com/Dimension/particles/Electron.java
@@ -0,0 +1,7 @@
+package com.Dimension.particles;
+
+/**
+ * Created by student on 1/18/18.
+ */
+public class Electron {
+}
---
+++ src/com/Dimension/particles/Quark.java
@@ -0,0 +1,20 @@
+package com.Dimension.particles;
+
+/**
+ * Created by student on 1/18/18.
+ */
+
+public class Quark {
+    public Quark () {
+
+    }
+}
+
+
+class upQuark extends Quark {
+
+}
+
+class downQuark extends Quark {
+
+}
