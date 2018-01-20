5e29e914f5b36471f4e0a976f8a2b5fc10ba8abf
Colin <colinm1215@gmail.com>
2018-01-20 16:39:53 -0500

    load

--- src/com/Dimension/particles/Proton.java
+++ src/com/Dimension/particles/Proton.java
@@ -6,10 +6,15 @@ public class Proton {
 
     public Proton () {
         int charge = 1;
-        List quarks = new ArrayList();
+        ArrayList<Quark> quarks = new ArrayList<>();
         quarks.add(new upQuark());
         quarks.add(new upQuark());
         quarks.add(new downQuark());
-
+        double tch = 0;
+        for (Quark q : quarks) {
+            System.out.println(q.toString());
+            tch += q.getCha();
+        }
+        System.out.println("Total Charge:" + tch);
     }
 }
